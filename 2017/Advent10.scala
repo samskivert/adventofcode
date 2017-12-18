1628840dabacd642824f9837699b44049638d23c
Michael Bayne <mdb@samskivert.com>
2017-12-18 15:33:52 -0800

    Fix bug with lack of leading 0s.
    Also factor knothash into function because we pilfer & reuse all this for day
    14.

--- 2017/Advent10.scala
+++ 2017/Advent10.scala
@@ -9,15 +9,17 @@ object Advent10 extends AdventApp {
   }
   def hashN (nums :Array[Int], lens :List[Int], n :Int) =
     (0 until n).foldLeft((0, 0)) { (ps, ii) => hash(nums, lens, ps._1, ps._2) }
+  def pad (len :Int)(digits :String) = "0" * (len-digits.length) + digits
+  def knothash (input :String) = {
+    val nums = (0 until 256).toArray
+    val lens = input.map(_.toInt).toList ++ List(17, 31, 73, 47, 23)
+    hashN(nums, lens, 64)
+    nums.grouped(16).map(_.reduce(_ ^ _)).map(_.toHexString).map(pad(2)).mkString("")
+  }
   val input = readline("data/input10.txt")
   def answer = ({
     val nums = (0 until 256).toArray
     hash(nums, input.split(",").map(_.toInt).toList, 0, 0)
     nums(0) * nums(1)
-  }, {
-    val nums = (0 until 256).toArray
-    val lens = input.map(_.toInt).toList ++ List(17, 31, 73, 47, 23)
-    hashN(nums, lens, 64)
-    nums.grouped(16).map(_.reduce(_ ^ _)).map(_.toHexString).mkString("")
-  })
+  }, knothash(input))
 }
