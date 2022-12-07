e5af8b25db46145fccf08e1cd516be62f3355498
Michael Bayne <mdb@samskivert.com>
2022-12-07 15:53:58 -0800

    OK, here's day six with no Swift Algorithms.

--- 2022/Sources/advent2022/Day6.swift
+++ 2022/Sources/advent2022/Day6.swift
@@ -3,12 +3,20 @@ import Algorithms
 struct Day6 : Day {
     let sampleInput = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
 
-    func allDifferent (code :String.SubSequence) -> Bool { Set(code).count == code.count }
     func startMarker (_ input :String, _ size :Int) -> Int {
         let windows = input.windows(ofCount: size)
-        return windows.distance(from: windows.startIndex, to: windows.firstIndex(where: allDifferent)!) + size
+        let first = windows.firstIndex(where: { Set($0).count == $0.count })!
+        return windows.distance(from: windows.startIndex, to: first) + size
     }
 
-    func part1 () throws -> String { String(startMarker(try readInput(6)[0], 4)) }
-    func part2 () throws -> String { String(startMarker(try readInput(6)[0], 14)) }
+    func startMarker2 (_ input :String, _ size :Int) -> Int {
+        for start in input.indices {
+            let end = input.index(start, offsetBy: size)
+            if Set(input[start..<end]).count == size { return input.distance(from: input.startIndex, to: end) }
+        }
+        return 0
+    }
+
+    func part1 () throws -> String { String(startMarker2(try readInput(6)[0], 4)) }
+    func part2 () throws -> String { String(startMarker2(try readInput(6)[0], 14)) }
 }
