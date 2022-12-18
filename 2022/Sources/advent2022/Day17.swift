a1674af13b661bf11c3e2bb392e0b07b6ae0efa1
Michael Bayne <mdb@samskivert.com>
2022-12-17 21:47:38 -0800

    Detect cycle properly.
    Cleaned up the code a bit, but I'm not going to golf it anymore. This is just
    for fun after all.

--- 2022/Sources/advent2022/Day17.swift
+++ 2022/Sources/advent2022/Day17.swift
@@ -14,7 +14,7 @@ struct Day17 : Day {
         var blank = [Int8](repeating: 0, count: width*100)
         var offset = 0
 
-        mutating func shiftDown () {
+        private mutating func shiftDown () {
             let width = Chamber.width
             if findEmpty(0) > 200 {
                 grid.replaceSubrange(0..<200*width, with: grid[100*width..<300*width])
@@ -23,18 +23,17 @@ struct Day17 : Day {
             }
         }
 
-        func cell (_ row :Int, _ col :Int) -> Int8 { grid[row*Chamber.width+col] }
-        mutating func setCell (_ row :Int, _ col :Int, _ v :Int8) { grid[row*Chamber.width+col] = v }
+        private func row (_ row :Int) -> ArraySlice<Int8> { grid.dropFirst(row*Chamber.width).prefix(Chamber.width) }
+        private func cell (_ row :Int, _ col :Int) -> Int8 { grid[row*Chamber.width+col] }
+        private mutating func setCell (_ row :Int, _ col :Int, _ v :Int8) { grid[row*Chamber.width+col] = v }
 
-        func occupied (_ row :Int) -> Bool {
-            var ii = 0 ; while ii < Chamber.width {
-                if cell(row, ii) != 0 { return true }
-                ii += 1
+        var height :Int { findEmpty(0) + offset }
+
+        private func findEmpty (_ fromRow :Int) -> Int {
+            func occupied (_ row :Int) -> Bool {
+                for cc in self.row(row) { if cc != 0 { return true }}
+                return false
             }
-            return false
-        }
-
-        func findEmpty (_ fromRow :Int) -> Int {
             var empty = fromRow ; while empty < Chamber.height {
                 if occupied(empty) { empty += 1 }
                 else { break }
@@ -42,13 +41,12 @@ struct Day17 : Day {
             return empty
         }
 
-        mutating func blow (_ pstart :Int, _ pheight :Int, _ wind :Character) {
+        private mutating func blow (_ pstart :Int, _ pheight :Int, _ wind :Character) {
             let left = wind == "<"
             let sc = left ? 0 : Chamber.width-1, ec = left ? Chamber.width-1 : 0
             let dc = left ? 1 : -1
-
-            var dr = 0 ; while dr < pheight {
-                let prow = pstart+dr
+            // if any cells on the edge of the piece cannot be blown over, do nothing
+            for prow in pstart ..< pstart+pheight {
                 var cc = sc ; while (cc != ec) {
                     if cell(prow, cc+dc) == 1 {
                         if cell(prow, cc) != 0 { return }
@@ -56,11 +54,9 @@ struct Day17 : Day {
                     }
                     cc += dc
                 }
-                dr += 1
             }
-
-            dr = 0 ; while dr < pheight {
-                let prow = pstart+dr
+            // otherwise shift everything over
+            for prow in pstart ..< pstart+pheight {
                 var cc = sc ; while (cc != ec) {
                     if cell(prow, cc+dc) == 1 {
                         setCell(prow, cc, 1)
@@ -68,56 +64,48 @@ struct Day17 : Day {
                     }
                     cc += dc
                 }
-                dr += 1
             }
         }
 
-        mutating func drop (_ pstart :Int, _ pheight :Int) -> Bool {
+        private mutating func drop (_ pstart :Int, _ pheight :Int) -> Bool {
             if pstart == 0 { return true }
-            var cc = 0 ; while (cc < Chamber.width) {
-                var rr = pstart ; while (rr < pstart+pheight) {
+            // if any cells on the bottom edge of the piece can't drop, we landed
+            for cc in 0 ..< Chamber.width {
+                for rr in pstart ..< pstart+pheight {
                     if cell(rr, cc) == 1 && cell(rr-1, cc) == 2 { return true }
-                    rr += 1
                 }
-                cc += 1
             }
-            var dr = 0 ; while dr < pheight {
-                var cc = 0 ; while (cc < Chamber.width) {
-                    if cell(pstart+dr, cc) == 1 {
-                        setCell(pstart+dr-1, cc, 1)
-                        setCell(pstart+dr, cc, 0)
+            for prow in pstart ..< pstart+pheight {
+                for cc in 0 ..< Chamber.width {
+                    if cell(prow, cc) == 1 {
+                        setCell(prow-1, cc, 1)
+                        setCell(prow, cc, 0)
                     }
-                    cc += 1
                 }
-                dr += 1
             }
             return false
         }
 
         mutating func add (piece :[[Int8]], _ wind :String, _ wpos :inout Int) {
             let firstEmpty = findEmpty(0)
-            let pheight = piece.count
             var pstart = firstEmpty+3
             var row = pstart ; for prow in piece {
-                var cc = 0, ll = prow.count ; while (cc < ll) {
-                    setCell(row, cc+2, prow[cc])
-                    cc += 1
-                }
+                let offset = row*Chamber.width + 2
+                grid.replaceSubrange(offset ..< offset+prow.count, with: prow)
                 row += 1
             }
 
+            let pheight = piece.count
             var landed = false
             while !landed {
                 blow(pstart, pheight, wind[wind.index(wind.startIndex, offsetBy: wpos)])
                 wpos = (wpos + 1) % wind.count
                 landed = drop(pstart, pheight)
                 if landed {
-                    var row = pstart ; for prow in piece {
-                        var cc = 0 ; while (cc < Chamber.width) {
+                    for row in pstart ..< pstart+piece.count {
+                        for cc in 0 ..< Chamber.width {
                             if cell(row, cc) == 1 { setCell(row, cc, 2) }
-                            cc += 1
                         }
-                        row += 1
                     }
                 } else { pstart -= 1 }
             }
@@ -130,37 +118,35 @@ struct Day17 : Day {
         var wpos = 0, ppos = 0
         var chamber = Chamber()
         for _ in 0 ..< 2022 {
-            chamber.add(piece: pieces[ppos % pieces.count], wind, &wpos)
-            ppos += 1
+            chamber.add(piece: pieces[ppos], wind, &wpos)
+            ppos = (ppos + 1) % pieces.count
         }
-        return String(chamber.findEmpty(0) + chamber.offset)
+        return String(chamber.height)
     }
 
     func part2 () throws -> String {
         let wind = try readInput(17)[0]
         var wpos = 0, ppos = 0
         var chamber = Chamber()
-        var cycle = 0, cheight = 0
+        var zeros = [(Int, Int, Int)]()
         let max = 1000000000000
         var ii = 0 ; while ii < max {
             chamber.add(piece: pieces[ppos], wind, &wpos)
             ii += 1
-            if ppos == 0 && wpos == 22 { // TODO: detect first cycle
-                let top = chamber.findEmpty(0)+chamber.offset
-                if cycle == 0 {
-                    cycle = ii
-                    cheight = top
-                } else {
-                    cycle = ii-cycle
-                    cheight = top-cheight
-                    let remain = max-ii
-                    let cycles = remain/cycle
-                    chamber.offset += cycles * cheight
+            if ppos == 0 && ii < max/2 {
+                let height = chamber.height
+                // if we see a repeat of the same wind position on piece 0 (twice in a row) then we've detected a cycle
+                if let cc = zeros.firstIndex(where: { $0.0 == wpos }), zeros[cc-1].0 == zeros.last!.0 {
+                    let (_, cii, cheight) = zeros[cc]
+                    let cycle = ii-cii, cycles = (max-ii)/cycle
+                    chamber.offset += cycles * (height-cheight)
                     ii += cycles * cycle
+                } else {
+                    zeros.append((wpos, ii, height))
                 }
             }
             ppos = (ppos + 1) % pieces.count
         }
-        return String(chamber.findEmpty(0) + chamber.offset)
+        return String(chamber.height)
     }
 }
