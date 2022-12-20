0a2d2db9b07cc29e3a7c2e7de1fdc22bf012915f
Michael Bayne <mdb@samskivert.com>
2022-12-20 09:29:41 -0800

    Added Swift Collections.
    I was all excited about using their BitArray implementation, but then I
    discovered that that's been "coming soon" for a year. Not the most rapidly
    progressing project, alas.

--- 2022/Package.swift
+++ 2022/Package.swift
@@ -6,15 +6,17 @@ import PackageDescription
 let package = Package(
     name: "advent2022",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
-        .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/apple/swift-algorithms", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         .executableTarget(
             name: "advent2022",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),                
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Algorithms", package: "swift-algorithms"),
+                .product(name: "Collections", package: "swift-collections"),
             ])
     ]
 )
