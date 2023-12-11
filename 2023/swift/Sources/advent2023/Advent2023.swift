f5e0e92049e3508cd9e05bb7ee749b7bb743e934
Michael Bayne <mdb@samskivert.com>
2023-12-11 09:44:38 -0800

    Day 11.
    Was baffled for a while why my answer to the second part was wrong before
    realizing that in part 1 I was adding 1 to expand space 2x and so in part 2 I
    should add 999999 to expand space 1000000x, _not_ add 1000000. Oops!
    
    Also hat tip to Swift Algorithms for providing combinations(). Saved me an
    annoyed nested reduce.

--- 2023/swift/Sources/advent2023/Advent2023.swift
+++ 2023/swift/Sources/advent2023/Advent2023.swift
@@ -31,6 +31,7 @@ struct Advent2023 : ParsableCommand {
     case 8: try compute(day, Day8(), inputA, inputB)
     case 9: try compute(day, Day9(), inputA, inputB)
     case 10: try compute(day, Day10(), inputA, inputB)
+    case 11: try compute(day, Day11(), inputA, inputB)
     default:
       print("No solution for day \(day) yet.")
     }
---
+++ 2023/swift/Sources/advent2023/Day11.swift
@@ -0,0 +1,25 @@
+struct Day11 : Day {
+
+  func parse (_ input :[String]) -> [(Int, Int)] {
+    input.enumerated().flatMap({ (y, line) in
+      line.enumerated().flatMap({ (x, c) in c == "#" ? [(x, y)] : [] })
+    })
+  }
+
+  func shift (_ galaxies :[(Int, Int)], _ shift :Int) -> [(Int, Int)] {
+    func exp (_ max :Int, _ gs :[Int]) -> [Int] {
+      (0 ... max).reductions(0, { (exp, v) in exp + (gs.contains(v) ? 0 : shift) })
+    }
+    let gxs = galaxies.map({ $0.0 }), xexp = exp(gxs.max()!, gxs)
+    let gys = galaxies.map({ $0.1 }), yexp = exp(gys.max()!, gys)
+    return galaxies.map({ gg in (gg.0 + xexp[gg.0+1], gg.1 + yexp[gg.1+1]) })
+  }
+
+  func dist (_ g1 :(Int, Int), _ g2 : (Int, Int)) -> Int { abs(g1.0-g2.0) + abs(g1.1-g2.1) }
+  func sumdists (_ galaxies :[(Int, Int)]) -> Int {
+    galaxies.combinations(ofCount: 2).map({ dist($0[0], $0[1]) }).reduce(0, +)
+  }
+
+  func part1 (_ input :[String]) -> Int { sumdists(shift(parse(input), 1)) }
+  func part2 (_ input :[String]) -> Int { sumdists(shift(parse(input), 999999)) }
+}
