8961f439d160aa1a93d4f3c580a479a390590df4
Michael Bayne <mdb@samskivert.com>
2024-12-19 17:59:18 -0800

    Day 19.
    I was not familiar with the "word break" problem/algorithm, so I beat my head
    against this for a while. I finally realized that if a more complex towel could
    be built from simpler towels, it could be ignored completely (for part one).
    That, combined with an efficient trie-based pattern checker got the job done
    for part one.
    
    Then I saw that in part two, we actually want to count up the hundreds of
    billions of ways that the patterns could be made. After beating my head against
    that for a while, I realized that this must just be a well established
    algorithm, so I asked ChatGPT what it was called and it told me.
    
    I don't feel too bad about not rediscovering this dynamic programming algorithm
    from scratch having no knowledge of it, but I am reminded that I should think
    in a more dynamic programming-y way when doing AoC problems. Invariably they
    will lean in that direction at least a couple of times in any given season.

--- 2024/rust/src/day19.rs
+++ 2024/rust/src/day19.rs
@@ -1,7 +1,27 @@
-pub fn part1(_input: &str) -> String {
-    "TODO".to_string()
+fn parse(input: &str) -> (Vec<&str>, Vec<&str>) {
+    let (towels, patterns) = input.split_once("\n\n").unwrap();
+    (towels.split(", ").collect::<Vec<_>>(), patterns.split("\n").collect::<Vec<_>>())
 }
 
-pub fn part2(_input: &str) -> String {
-    "TODO".to_string()
+fn count_ways(towels: &[&str], pattern: &str) -> usize {
+    let mut ways = vec![0; pattern.len() + 1];
+    ways[0] = 1;
+    for i in 0..pattern.len() {
+        for towel in towels.iter() {
+            if pattern[i..].starts_with(towel) {
+                ways[i + towel.len()] += ways[i];
+            }
+        }
+    }
+    ways[pattern.len()]
+}
+
+pub fn part1(input: &str) -> String {
+    let (towels, patterns) = parse(input);
+    patterns.iter().filter(|p| count_ways(&towels, p) > 0).count().to_string()
+}
+
+pub fn part2(input: &str) -> String {
+    let (towels, patterns) = parse(input);
+    patterns.iter().map(|p| count_ways(&towels, p)).sum::<usize>().to_string()
 }
