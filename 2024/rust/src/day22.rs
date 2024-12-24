7dd4eb8a1050820e982ce8ba90ee3b0b9deebc07
Michael Bayne <mdb@samskivert.com>
2024-12-24 14:40:14 -0800

    Day 22.
    Surprisingly uncomplicated. The bit twiddling was a nice little lark, and then
    the search for the best price was straightforward.
    
    It would have been even more straightforward had I realized in the beginning
    that my cute idea of sorting the price sequences from highest to lowest was
    invalid. A monkey will sell on the _first_ occurrence of a given sequence, so
    we need them in their naturally occuring order.

--- 2024/rust/src/day22.rs
+++ 2024/rust/src/day22.rs
@@ -1,7 +1,52 @@
-pub fn part1(_input: &str) -> String {
-    "TODO".to_string()
+fn next(seed: u64) -> u64 {
+    let seed = (seed ^ (seed * 64)) % 16777216;
+    let seed = (seed ^ (seed / 32)) % 16777216;
+    let seed = (seed ^ (seed * 2048)) % 16777216;
+    seed
 }
 
-pub fn part2(_input: &str) -> String {
-    "TODO".to_string()
+struct Seq {
+    deltas: [i8; 4],
+    price: u16,
+}
+
+fn seqs_n(seed: u64, n: u64) -> Vec<Seq> {
+    let mut seed = seed;
+    let mut seqs = Vec::new();
+    let mut deltas = [0, 0, 0, 0];
+    for ii in 0..n - 1 {
+        let price = (seed % 10) as i8;
+        seed = next(seed);
+        let nprice = (seed % 10) as i8;
+        let delta = nprice - price;
+        deltas[0] = deltas[1];
+        deltas[1] = deltas[2];
+        deltas[2] = deltas[3];
+        deltas[3] = delta;
+        if ii > 2 {
+            seqs.push(Seq { price: nprice as u16, deltas });
+        }
+    }
+    seqs
+}
+
+pub fn part1(input: &str) -> String {
+    let seeds = input.lines().map(|line| line.parse::<u64>().unwrap());
+    seeds.map(|s| (0..2000).fold(s, |s, _| next(s))).sum::<u64>().to_string()
+}
+
+fn find_best(seqss: &[Vec<Seq>], seq: &Seq, total: u16) -> u16 {
+    if seqss.len() == 0 {
+        total
+    } else if let Some(nseq) = seqss[0].iter().find(|nseq| seq.deltas == nseq.deltas) {
+        find_best(&seqss[1..], seq, total + nseq.price)
+    } else {
+        find_best(&seqss[1..], seq, total)
+    }
+}
+
+pub fn part2(input: &str) -> String {
+    let seeds = input.lines().map(|line| line.parse::<u64>().unwrap());
+    let seqss = seeds.map(|s| seqs_n(s, 2000)).collect::<Vec<_>>();
+    seqss[0].iter().map(|seq| find_best(&seqss[1..], seq, seq.price)).max().unwrap().to_string()
 }
