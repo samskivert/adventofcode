6c6dc3f4c6943790b643d6cd822f88e936052982
Michael Bayne <mdb@samskivert.com>
2024-12-25 20:55:18 -0800

    Day 24.
    Part two was not readily amenable to algorithmic solution. I did actually take
    a crack and wrote a search algorithm which relied on the cascading structure of
    a binary adder. It found the lowest bit that was incorrect, then looked at the
    dependencies of that output bit and tried swapping them all. The expectation
    was that something upstream of the incorrect bit would have to be out of place.
    
    That didn't quite work. If the bit itself was out of place, it could have been
    swapped with something that was not a dependency of that bit.
    
    I wrote some code to generate a graph of the circuit and eyeballed the first
    invalid bit and saw that it had indeed been swapped with something a bit
    downstream of its own dependencies.
    
    I didn't feel like bothering to write code to carve out the "neighborhood"
    around an output bit to search for swap candidates. It was easy enough to grok
    the general structure of the adder they were using and then visually scan the
    graph to find the out of place nodes.
    
    It's my party and I'll use my eyes if I want to.

--- 2024/rust/src/day24.rs
+++ 2024/rust/src/day24.rs
@@ -1,7 +1,161 @@
-pub fn part1(_input: &str) -> String {
-    "TODO".to_string()
+use std::collections::HashMap;
+
+static GLYPHS: &str = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+fn to_code(s: &str) -> u16 {
+    s.chars().fold(0, |acc, c| acc * 36 + GLYPHS.find(c).unwrap() as u16)
+}
+// fn from_code(c: u16) -> String {
+//     let mut s = String::new();
+//     let mut c = c;
+//     for _ in 0..3 {
+//         s.push(GLYPHS.chars().nth(c as usize % 36).unwrap());
+//         c /= 36;
+//     }
+//     s.chars().rev().collect()
+// }
+
+#[derive(Debug, Clone)]
+enum Op {
+    And,
+    Or,
+    Xor,
 }
 
-pub fn part2(_input: &str) -> String {
-    "TODO".to_string()
+#[derive(Debug, Clone)]
+struct Gate {
+    a: u16,
+    b: u16,
+    op: Op,
+    out: u16,
+}
+
+fn parse(input: &str) -> (HashMap<u16, u8>, Vec<Gate>) {
+    let (inits, gates) = input.split_once("\n\n").unwrap();
+    let inits = inits
+        .lines()
+        .map(|line| line.split_once(':').unwrap())
+        .map(|(k, v)| (to_code(k), v.trim().parse().unwrap()))
+        .collect();
+    let gates = gates
+        .lines()
+        .map(|line| {
+            let mut parts = line.split(' ');
+            let a = to_code(parts.next().unwrap());
+            let opstr = parts.next().unwrap();
+            let b = to_code(parts.next().unwrap());
+            parts.next(); // skip ->
+            let out = to_code(parts.next().unwrap());
+            let op = match opstr {
+                "AND" => Op::And,
+                "OR" => Op::Or,
+                "XOR" => Op::Xor,
+                _ => panic!("Unknown op: {}", opstr),
+            };
+            Gate { a, b, op, out }
+        })
+        .collect();
+    (inits, gates)
+}
+
+fn read_reg(state: &HashMap<u16, u8>, reg: &str) -> usize {
+    let mut out: usize = 0;
+    let mut bit = 0;
+    while let Some(v) = state.get(&to_code(&format!("{}{:02}", reg, bit))) {
+        out += (*v as usize) << bit;
+        bit += 1;
+    }
+    out
+}
+
+fn eval(state: &HashMap<u16, u8>, gates: &[Gate]) -> usize {
+    let mut gates = gates.to_vec();
+    let mut state = state.clone();
+    while !gates.is_empty() {
+        let plen = gates.len();
+        gates.retain(|gate| {
+            if let Some(a) = state.get(&gate.a) {
+                if let Some(b) = state.get(&gate.b) {
+                    let out = match gate.op {
+                        Op::And => a & b,
+                        Op::Or => a | b,
+                        Op::Xor => a ^ b,
+                    };
+                    state.insert(gate.out, out);
+                    false
+                } else {
+                    true
+                }
+            } else {
+                true
+            }
+        });
+        if gates.len() == plen {
+            return 0;
+        }
+    }
+    read_reg(&state, "z")
+}
+
+pub fn part1(input: &str) -> String {
+    let (state, gates) = parse(input);
+    eval(&state, &gates).to_string()
+}
+
+fn swap(gates: &mut Vec<Gate>, a: &str, b: &str) {
+    let ac = to_code(a);
+    let bc = to_code(b);
+    let aidx = gates.iter().position(|g| g.out == ac).unwrap();
+    let bidx = gates.iter().position(|g| g.out == bc).unwrap();
+    let tmp = gates[aidx].out;
+    gates[aidx].out = gates[bidx].out;
+    gates[bidx].out = tmp;
+}
+
+fn check(state: &HashMap<u16, u8>, gates: &[Gate]) -> bool {
+    let x = read_reg(&state, "x");
+    let y = read_reg(&state, "y");
+    let xy = x + y;
+    let z = eval(&state, &gates);
+
+    if xy == z {
+        true
+    } else {
+        for bit in 0..47 {
+            let mask = (1 << (bit + 1)) - 1;
+            if z & mask != xy & mask {
+                println!("differs at bit {}", bit);
+                break;
+            }
+        }
+        false
+    }
+}
+
+// fn print_dot(gates: &[Gate]) {
+//     println!("digraph G {{");
+//     for gate in gates {
+//         let (shape, sym) = match gate.op {
+//             Op::And => ("triangle", "&"),
+//             Op::Or => ("invtriangle", "|"),
+//             Op::Xor => ("invhouse", "^"),
+//         };
+//         let gate_id = format!("{}", from_code(gate.out));
+//         println!("  {} [label=\"{} {}\", shape={}]", gate_id, gate_id, sym, shape);
+//         println!("  {} -> {}", from_code(gate.a), gate_id);
+//         println!("  {} -> {}", from_code(gate.b), gate_id);
+//     }
+//     println!("}}");
+// }
+
+pub fn part2(input: &str) -> String {
+    let (state, mut gates) = parse(input);
+    swap(&mut gates, "z15", "qnw");
+    swap(&mut gates, "z20", "cqr");
+    swap(&mut gates, "nfj", "ncd");
+    swap(&mut gates, "z37", "vkg");
+    check(&state, &gates);
+    let mut swaps = ["z15", "qnw", "z20", "cqr", "nfj", "ncd", "z37", "vkg"];
+    swaps.sort();
+    swaps.join(",")
 }
